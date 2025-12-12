a35e7075177bf061d54465293b5bb0a9b4cad2d0
Daniela Gutierrez Aliaga <daniela_gutierrez_aliaga@hotmail.com>
2025-12-12 21:42:28 +0100

    Afegida funcionalitat de Marta

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 console.log('Proyecto ModaGlobal cargado correctamente');
 console.log('Nueva funcionalidad añadida por Daniela');
+//Añadido por Marta
+if ("".trim() === "") {
+    console.log("No se pueden añadir contactos vacíos.");
+}
+
